5a8634f5d88946ffebe1980685777756dbd3bf7d
Thomas Boda <boda.thomas@orange.fr>
2021-02-24 01:16:44 +0100

    Add the TMDB API initialization

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import './App.css'
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <div className="title">Last Movies</div>
-            </header>
-        </div>
-    )
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <div className="title">Last Movies</div>
+                </header>
+            </div>
+        )
+    }
 }
 
 export default App
